6ce91d44e7dfbb61b8e79090a37043ab1fceafff
Frank Perez <frank@zcuadro.com>
2015-04-09 23:13:50 -0400

    Added settings to deploy app

---
+++ config/unicorn.rb
@@ -0,0 +1,30 @@
+#  unicorn.rb
+#  
+#  Copyright 2015 Frank Perez <frank@Pluto>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+root = "opt/www/testapp/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.testapp.sock"
+worker_processes 1
+timeout 30
